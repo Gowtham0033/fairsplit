9b1c5c846cad9318fe91a9fb0fe76eea375706c9
Gowtham0033 <126399059+Gowtham0033@users.noreply.github.com>
2025-02-12 16:01:18 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,17 +5,24 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const { user, login, logout } = useContext(UserContext);
   return (
-    <div className="bg-green-500 text-white  ">
+    <div
+      className={`  text-white ${
+        user ? " bg-green-400 mb-2" : "p-6 bg-green-100"
+      }`}
+    >
       <div>
         {user ? (
           <div className="flex items-center justify-between ">
-            <p>Welcome back {user.username}</p>
-            <button
-              className="bg-orange-600 px-4 py-1 text-md text-white m-2"
-              onClick={() => logout()}
-            >
-              Logout
-            </button>
+            <div className="ml-4">Splitwise</div>
+            <div className="flex items-center justify-center">
+              <p> {user.username}</p>
+              <button
+                className="bg-orange-600 px-4 py-1 text-md text-white m-2"
+                onClick={() => logout()}
+              >
+                Logout
+              </button>
+            </div>
             {/* <Link to={"/all"}>
               <div className="bg-slate-400 w-[100px] p-1 m-1" >All Expenses</div>
             </Link> */}
@@ -27,7 +34,34 @@ const Header = () => {
             {/* <AddTransaction friendId={null} /> */}
           </div>
         ) : (
-          <Link to={"/login"}>Login</Link>
+          <div className="flex justify-between items-center">
+            <div className="ml-4 text-xl text-green-00 underline">Splitwise</div>
+            <div className="text-end">
+              <Link
+                to={"/login"}
+                style={{
+                  color: "#1CC29F",
+                  fontSize: "16 px",
+                }}
+              >
+                Login
+              </Link>
+              <Link
+                to={"/register"}
+                className="ml-8 mr-20"
+                style={{
+                  color: "white",
+                  background: "#1CC29F",
+                  padding: "10px 14px",
+                  fontSize: "16px",
+                  borderRadius: "6px",
+                  transition: "background-color 1s ease",
+                }}
+              >
+                Sign up
+              </Link>
+            </div>
+          </div>
           // <Login/>
           // <p>Logged in as guest</p>
           //   <Login />
